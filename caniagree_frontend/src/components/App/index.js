7c8bc37902ed500188a4d077a4982ad052cbc94b
William R J  Ribeiro <william.ribeiro@talkdesk.com>
2017-09-29 12:14:54 +0100

    Uses Reactstrap components
    For basic layout and search form

--- caniagree_frontend/src/components/App/index.js
+++ caniagree_frontend/src/components/App/index.js
@@ -1,29 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Search from '../../containers/search'
+import { Container, Row, Col } from 'reactstrap'
 
 const App = ({ services }) => (
-  <div>
-    <p>cenas</p>
-    <Search />
-    {
-      services.map(({self, name, id}) => (
-        <div key={id}>
-          <span>{self}</span>
-          <span>{name}</span>
-          <span>{id}</span>
-        </div>
-      ))
-    }
-  </div>
+  <Container>
+    <Row>
+      <Col>
+        <h1>Can I Agree?</h1>
+      </Col>
+    </Row>
+
+    <Row>
+      <Col>
+        <Search />
+      </Col>
+    </Row>
+
+    <Row>
+      <Col>
+        {services.map(({ self, name, id }) => (
+          <div key={id}>
+            <span>{self}</span>
+            <span>{name}</span>
+            <span>{id}</span>
+          </div>
+        ))}
+      </Col>
+    </Row>
+  </Container>
 )
 
 App.propTypes = {
-  services: PropTypes.arrayOf(PropTypes.shape({
-    self: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired
-  })).isRequired
+  services: PropTypes.arrayOf(
+    PropTypes.shape({
+      self: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired
+    })
+  ).isRequired
 }
 
 export default App
